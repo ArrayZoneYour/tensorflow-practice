18c53817f3fc6132bdad5bf099a80c34726a6fc6
ArrayZoneYour <hustliyidong@gmail.com>
2017-12-26 20:00:40 +0800

    高维情况下的线性回归实现以及噪声的尝试分析（没有归一化，感觉不行）

--- Regression/CircleNoiseEffiect.py
+++ Regression/CircleNoiseEffiect.py
@@ -10,16 +10,44 @@ from tensorflow.python.framework import ops
 ops.reset_default_graph()
 sess = tf.Session()
 
-# 鸢尾花数据集
-iris = datasets.load_iris()
-x_vals = np.array([x[3] for x in iris.data])
-y_vals = np.array([y[0] for y in iris.data])
 
-learning_rate = 0.05
+# 对称圆数据集
+# 半径为1(cos x, sin x)的圆随机扰动
+# base采集点为（100，200）
+x_vals = np.cos(np.random.normal(0, 1, 100))
+y_vals = [np.random.choice([1, -1], size=1)[0] * np.sqrt(1 - x_val ** 2) for x_val in x_vals]
+x_vals = np.array([x_val + 100 for x_val in x_vals])
+y_vals = np.array([y_vals + 200 for y_vals in y_vals])
+
+
+# 归一化
+# def normalize_cols(m):
+#     col_max = m.max(axis=0)
+#     col_min = m.min(axis=0)
+#     return (m-col_min) / (col_max - col_min)
+#
+#
+# x_vals = np.nan_to_num(normalize_cols(x_vals))
+# y_vals = np.nan_to_num(normalize_cols(y_vals))
+
+plt.plot(x_vals, y_vals, 'o', label='Data Points')
+plt.show()
+
+# iris 数据集超参数
+# learning_rate = 0.05
+# batch_size = 25
+
+# 对称圆数据集超参数
+learning_rate = 0.001
 batch_size = 25
 
 x_data = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+# Perfect initialize
+# A = tf.Variable(tf.constant([[2.]]))
+# b = tf.Variable(tf.constant([[0.]]))
+
+# Simple initialize
 A = tf.Variable(tf.random_normal(shape=[1, 1]))
 b = tf.Variable(tf.random_normal(shape=[1, 1]))
 
@@ -36,7 +64,7 @@ my_opt = tf.train.GradientDescentOptimizer(learning_rate)
 train_step = my_opt.minimize(loss)
 
 loss_vec = []
-for i in range(150):
+for i in range(1000):
     rand_index = np.random.choice(len(x_vals), size=batch_size)
     rand_x = np.transpose([x_vals[rand_index]])
     rand_y = np.transpose([y_vals[rand_index]])
@@ -49,17 +77,21 @@ for i in range(150):
         y_target: rand_y
     })
     loss_vec.append(temp_loss)
-    if (i + 1) % 10 == 0:
-        print('Loss:' + str(temp_loss))
+    if (i + 1) % 10 == 0 or i < 10:
+        [temp_slope] = sess.run(A)
+        [temp_y_intercept] = sess.run(b)
+        print('Loss:' + str(temp_loss) + 'slope' + str(temp_slope) + 'intercept' + str(temp_y_intercept))
 
 [slope] = sess.run(A)
 [y_intercept] = sess.run(b)
 best_fit = []
-for i in x_vals:
+x_s = np.linspace(0, 100, 100)
+for i in x_s:
+    print(i, slope * i + y_intercept)
     best_fit.append(slope * i + y_intercept)
 
 plt.plot(x_vals, y_vals, 'o', label='Data Points')
-plt.plot(x_vals, best_fit, 'r-', label='Best Fit Line')
+plt.plot(x_s, best_fit, 'r-', label='Best Fit Line')
 plt.legend(loc='upper left')
 plt.title('Sepal Length vs Pedal width')
 plt.xlabel('Pedal Width')
--- Regression/RegressionHighDim.py
+++ Regression/RegressionHighDim.py
@@ -1,19 +1,21 @@
 # /usr/bin/python
 # coding: utf-8
 # 基于矩阵分解的回归算法实现
-# TensorFlow的矩阵求逆也是使用Cholesky分解实现的
 
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
 sess = tf.Session()
-x_vals = np.linspace(0, 10, 100)
-y_vals = x_vals + np.random.normal(0, 1, 100)
+x_vals = np.linspace(0, 10, 100000)
+x_vals = x_vals.reshape([100, 1000])
+# y_vals = np.array([np.average(arr) for arr in x_vals]) + np.random.normal(0, 1, 1000)
 
 x_vals_column = np.transpose(np.matrix(x_vals))
-ones_column = np.transpose(np.matrix(np.repeat(1, 100)))
+ones_column = np.transpose(np.matrix(np.repeat(1, 1000)))
 A = np.column_stack((ones_column, x_vals_column))
+
+y_vals = np.array([np.average(arr) for arr in x_vals_column]) + np.random.normal(0, 1, 1000)
 b = np.transpose(np.matrix(y_vals))
 
 A_tensor = tf.constant(A)
@@ -23,6 +25,7 @@ tA_A = tf.matmul(tf.transpose(A_tensor), A_tensor)
 tA_A_inv = tf.matrix_inverse(tA_A)
 product = tf.matmul(tA_A_inv, tf.transpose(A_tensor))
 solution = tf.matmul(product, b_tensor)
+
 solution_eval = sess.run(solution)
 
 slope = solution_eval[1][0]
@@ -35,3 +38,4 @@ plt.plot(x_vals, y_vals, 'o', label='Data')
 plt.plot(x_vals, best_fit, 'r-', label='Best fit line', linewidth=3)
 plt.legend(loc='upper left')
 plt.show()
+
